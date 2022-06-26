a238216190d87c2ff61c068ceca7e0680fd3de4f
Aliasghar Asgharinia <a.asgharinia@yahoo.com>
2022-06-26 19:05:48 +0200

    Version 1

---
+++ CarrLoan.java
@@ -0,0 +1,51 @@
+public class CarrLoan {
+        int carLoan;
+        int loanLength;
+        int interestRate;
+        int downPayment;
+        public CarrLoan(int loanCar,int LengthLoan, int rateInterest, int paymentDown){
+            this.carLoan = loanCar;
+            this.loanLength = LengthLoan;
+            this.interestRate = rateInterest;
+            this.downPayment = paymentDown;
+        }
+        public boolean rateCheck(){
+            if(this.loanLength <=0 || this.interestRate <=0){
+                return false;
+                /*System.out.println("Error! You must take out a valid car loan.");*/
+            } else {
+                return true;
+            }
+        }
+        public boolean downPaymentCheck(){
+            if(this.downPayment > this.carLoan){
+                return true;
+                /*  System.out.println("The car can be paid in full."); */
+            }
+            else {
+                /* System.out.println("Come to the bank for your car"); */
+                return false;
+            }
+        }
+        public int remainingBalance(){
+            boolean checkRate = rateCheck();
+            boolean paymentCheckDown = downPaymentCheck();
+            if(checkRate && paymentCheckDown ) {
+                System.out.println("Come to the bank");
+                System.out.println("The car can be paid in full."); return 1;
+            } else if(!checkRate && !paymentCheckDown) {
+                System.out.println("Error! You must take out a valid car loan."); return 2;
+            } else {
+                int remainingBalance = this.carLoan - this.downPayment;
+                return remainingBalance;
+            }
+        }
+    public double remainingBalanceMonthly(){
+        double monthlyPayment;
+        double remaining = remainingBalance();
+        double remainMonth = this.loanLength * 12;
+        monthlyPayment = remaining /remainMonth;
+        return  monthlyPayment;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        CarrLoan loan = new CarrLoan(10000,3,5,1000);
+        loan.rateCheck();
+        int remaining = loan.remainingBalance();
+        System.out.println("Remaining = " +remaining );
+        CarrLoan loan1 = new CarrLoan(10000,2,3,2000);
+        loan1.rateCheck();
+        int remaining1 = loan1.remainingBalance();
+        System.out.println("Remaining1 = " +remaining1 );
+        double monthlyPayment1 = loan1.remainingBalanceMonthly();
+        System.out.println("Monthly Payment "+monthlyPayment1);
+        CarrLoan loan2 = new CarrLoan(10000,0,0,3000);
+        loan2.rateCheck();
+        int remaining2 = loan2.remainingBalance();
+        System.out.println("Remaining2 = " +remaining2 );
+        double monthlyPayment2 = loan2.remainingBalanceMonthly();
+        System.out.println("Monthly Payment "+monthlyPayment2);
+    }
+}
